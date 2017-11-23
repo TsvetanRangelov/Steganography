7835a0c1c449db610cb39026f9216ad0ffee5619
TsvetanRangelov <2137876r@bo1028-3-10u.dcs.gla.ac.uk>
2017-11-23 15:59:05 +0000

    Improved code commentary

--- stego.c
+++ stego.c
@@ -4,25 +4,33 @@
 #include "qdbmp.h"
 #include <time.h>
 
+
+//Max power for a ULONG, helper const = 2^63
 const UINT UINTPOW = 9223372036854775808;
 
 
+//Returns the GrayCode encoding for a picture
 char binaryToGray(char c){
   return c ^ (c >> 1);
 }
 
+
+//Prints a char/pixel as its bits
 void printbits(char v) {
   int i;
   for(i = 7; i >= 0; i--) putchar('0' + ((v >> i) & 1));
   putchar(' ');
 }
 
+
+//Helper power function for UINT to prevent casting
 UINT pow(UINT base, UINT power){
   if(power==0) return 1;
   return base*pow(base,power-1);
 }
 
 
+//Returns the noise in the given plane
 int calc_noise(UINT plane){
   int rows[8];
   int i, j, noise=0;
@@ -38,6 +46,8 @@ int calc_noise(UINT plane){
   return noise;
 }
 
+
+//Counts the number of transitions between two rows
 int row_noise(int num){
   int noise=0, i;
   int prev = num%2;
@@ -49,6 +59,8 @@ int row_noise(int num){
   return noise;
 }
 
+//Counts the number of transitions between ones and zeros
+//in a row
 int count_ones(int num){
   int i, ones=0;
   for(i=0;i<sizeof(int);++i){
@@ -58,6 +70,8 @@ int count_ones(int num){
   return ones;
 }
 
+
+//Takes two bmp files and outputs difference to a third one
 void bmp_diff(BMP *pic1, BMP *pic2, BMP *out){
   UCHAR pixels1[3], pixels2[3];
   UINT width = BMP_GetWidth(pic1);
@@ -99,7 +113,10 @@ void encrypt_sector(BMP *bmp, UINT x, UINT y, UINT sectorSize){
       }
     }
   }
+  
 
+  //Encrypts the data, currently with a random data pattern
+  //!!!Should be refactored
   for(i=0;i<8;++i){
     for(j=0;j<3;++j){
       if(calc_noise(planes[i][j])>40){
@@ -126,6 +143,8 @@ void encrypt_sector(BMP *bmp, UINT x, UINT y, UINT sectorSize){
   }
 }
 
+//General picture wrapper that breaks down the picture into suitable
+//sizes for computation
 void encrypt_picture(BMP *bmp, UINT sectorSize){
   UINT width = BMP_GetWidth(bmp);
   UINT height = BMP_GetHeight(bmp);
@@ -147,19 +166,24 @@ int main(int argc, char *argv[]){
   BMP_CHECK_ERROR(stdout, -1);
 
   printf("Height: %d, Width: %d, Depth in bits: %d\n", BMP_GetHeight(bmp), BMP_GetWidth(bmp), BMP_GetDepth(bmp));
-
+  
+  //There are 3 arguments given, comparing differences.
   if(argc==4){
     BMP* pic1 = BMP_ReadFile(argv[1]);
     BMP* pic2 = BMP_ReadFile(argv[2]);
     BMP* out = BMP_Create(BMP_GetWidth(pic1), BMP_GetHeight(pic1), 24);
     bmp_diff(pic1,pic2,out);
     BMP_WriteFile(out, argv[3]);
+    BMP_Free(pic1);
+    BMP_Free(pic2);
+    BMP_Free(out);
   }
-  else  encrypt_picture(bmp, 8);
-
-
-  BMP_WriteFile(bmp, "airplane.bmp");
-  BMP_CHECK_ERROR(stdout, -1);
-  BMP_Free(bmp);
+  //Encrypting the file as per standard stego
+  else{  
+    encrypt_picture(bmp, 8);
+    BMP_WriteFile(bmp, "airplane.bmp");
+    BMP_CHECK_ERROR(stdout, -1);
+    BMP_Free(bmp);
+  }
   return 0;
 }
